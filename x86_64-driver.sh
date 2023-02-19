caa8e795bab917ef5f6aa74c8595ac641fea0aa9
writemorecode <gustav.ae.karlsson@gmail.com>
2023-02-19 19:38:25 +0100

    Cleaned up x86_64 driver shell script

--- x86_64-driver.sh
+++ x86_64-driver.sh
@@ -11,20 +11,11 @@ PROGRAM_NAME=$(echo "$BASENAME" | cut -d '.' -f1)
 NEW_NAME="$PROGRAM_NAME.s"
 touch "$NEW_NAME"
 
-echo ".intel_syntax noprefix" > "$NEW_NAME"
-echo ".data" >> "$NEW_NAME"
-echo 'fmt_str: .asciz "%d\n"' >> "$NEW_NAME"
-echo -e "ARRAY:\n.quad 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0\n"  >> "$NEW_NAME"
-
-echo ".text" >> "$NEW_NAME"
-echo ".global main" >> "$NEW_NAME" 
-echo "main:" >> "$NEW_NAME"
+cat code/header.s > "$NEW_NAME"
 
 ./code/calc3b.exe < "$1" >> "$NEW_NAME"
 
-echo -e "\tmov\trax, 60" >> "$NEW_NAME"
-echo -e "\tmov\trdi, 0" >> "$NEW_NAME"
-echo -e "\tsyscall" >> "$NEW_NAME"
+cat code/footer.s >> "$NEW_NAME"
 
 gcc -L code/extras -ggdb3 "$NEW_NAME" -o "$PROGRAM_NAME" -lfactorial -llntwo -lgcd
 
