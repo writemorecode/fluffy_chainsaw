1afc6a9668398e6bc92926ccf04ea9d858d2f337
writemorecode <gustav.ae.karlsson@gmail.com>
2023-08-29 17:45:06 +0200

    Shell scripts now pass shellcheck

--- x86_64-driver.sh
+++ x86_64-driver.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
+if [[ "$#" -ne 1 ]]; then
     echo "Usage: $0 <program.calc>"
     exit 1
 fi
 
 BASENAME=$(basename "$1")
-PROGRAM_NAME=$(echo "$BASENAME" | cut -d '.' -f1)
+PROGRAM_NAME=$(echo "${BASENAME}" | cut -d '.' -f1)
 
-NEW_NAME="$PROGRAM_NAME.s"
-touch "$NEW_NAME"
+NEW_NAME="${PROGRAM_NAME}.s"
+touch "${NEW_NAME}"
 
-cat code/header.s > "$NEW_NAME"
+cat code/header.s > "${NEW_NAME}"
 
-./code/calc3i.exe < "$1" >> "$NEW_NAME"
+./code/calc3i.exe < "$1" >> "${NEW_NAME}"
 
-cat code/footer.s >> "$NEW_NAME"
+cat code/footer.s >> "${NEW_NAME}"
 
-gcc -L code/extras -ggdb3 "$NEW_NAME" -o "$PROGRAM_NAME" -lfactorial -llntwo -lgcd -lprint
+gcc -L code/extras -ggdb3 "${NEW_NAME}" -o "${PROGRAM_NAME}" -lfactorial -llntwo -lgcd -lprint
 
