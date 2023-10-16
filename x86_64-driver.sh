8b7fb6721c23bc832d23d170ddc8b267f1962ad9
writemorecode <gustav.ae.karlsson@gmail.com>
2023-10-16 19:52:18 +0200

    Updated x86_64-driver.sh for renamed files

--- x86_64-driver.sh
+++ x86_64-driver.sh
@@ -11,11 +11,11 @@ PROGRAM_NAME=$(echo "${BASENAME}" | cut -d '.' -f1)
 NEW_NAME="${PROGRAM_NAME}.s"
 touch "${NEW_NAME}"
 
-cat lexyacc-code/header.s > "${NEW_NAME}"
+cat lexyacc-code/header.txt > "${NEW_NAME}"
 
 bin/calc3i.exe < "$1" >> "${NEW_NAME}"
 
-cat lexyacc-code/footer.s >> "${NEW_NAME}"
+cat lexyacc-code/footer.txt >> "${NEW_NAME}"
 
 gcc -L lib/ "${NEW_NAME}" -o "${PROGRAM_NAME}" -lfunctions
 
